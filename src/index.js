af48b99584603af187ee8da734cf47a739abda43
tummeito <tummeito@gmail.com>
2022-12-13 11:28:44 -0800

    first init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import PocketBase from 'pocketbase'
+
+const USERNAME = "tummeito@csu.fullerton.edu";
+const PASSWORD = "cpsc349project4";
+const pb = new PocketBase("http://127.0.0.1:8090");
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD);
+console.log(authData);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
