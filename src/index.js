bb47ed56a67800012821ca3077ad8e5340a5775f
Thomas Phan <47121602+tummeito@users.noreply.github.com>
2022-12-13 12:39:03 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import PocketBase from 'pocketbase'
 
-const USERNAME = "tummeito@csu.fullerton.edu";
-const PASSWORD = "cpsc349project4";
-const pb = new PocketBase("http://127.0.0.1:8090");
-const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD);
-console.log(authData);
+async function authenticate() {
+    const USERNAME = "tummeito@csu.fullerton.edu";
+    const PASSWORD = "cpsc349project4";
+    const pb = new PocketBase("http://127.0.0.1:8090");
+    const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD);
+}
 
+authenticate();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
